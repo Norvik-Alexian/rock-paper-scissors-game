7a58e9d1064c6a3fad581b120c71dfd6aa2b1b47
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-02-13 16:29:49 +0400

    changing the concept of the code to OOP.

--- script-2.py
+++ script-2.py
@@ -4,69 +4,86 @@ from random import choice
 
 game_choices = ['rock', 'paper', 'scissors']
 
-correct_count = 0
-prev_choice = None
-prev_prev_choice = None
-prev_prev_prev_choice = None
-combinations = list(itertools.product(game_choices, repeat=4))
 
-combination_history = {combination: 0 for combination in combinations}
+class Game:
+    def __init__(self):
+        self.correct_count = 0
+        self.prev_choice = None
+        self.prev_prev_choice = None
+        self.prev_prev_prev_choice = None
 
-for i in range(7):
-    computer_choice = choice(game_choices)
-    user_choice = input('Enter your choice: ')
-    print(f'Computer chose => {computer_choice}')
+    def collecting_history(self):
+        combinations = list(itertools.product(game_choices, repeat=4))
+        combination_history = {combination: 0 for combination in combinations}
 
-    if computer_choice == user_choice:
-        correct_count += 1
-    if i > 2:
-        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, user_choice)] += 1
+        for i in range(7):
+            computer_choice = choice(game_choices)
+            user_choice = input('Enter your choice: ')
+            print(f'Computer chose => {computer_choice}')
 
-    prev_prev_prev_choice = prev_prev_choice
-    prev_prev_choice = prev_choice
-    prev_choice = user_choice
+            if computer_choice == user_choice:
+                self.correct_count += 1
+            if i > 2:
+                combination_history[(self.prev_prev_prev_choice, self.prev_prev_choice, self.prev_choice, user_choice)] += 1
 
-for i in range(7):
-    prediction = None
-    user_choice = input('Enter your choice: ')
-    print(f'Computer chose => {computer_choice}')
+            self.prev_prev_prev_choice = self.prev_prev_choice
+            self.prev_prev_choice = self.prev_choice
+            self.prev_choice = user_choice
 
-    if i > 2:
-        if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')] > combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')]:
-            prediction = 'rock'
-        else:
-            prediction = 'paper'
+    def guess_possiblity(self):
+        prediction = None
+        for i in range(7):
+            user_choice = input('Enter your choice: ')
+            print(f'Computer chose => {computer_choice}')
 
-        if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')] > combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')]:
-            prediction = 'rock'
-        else:
-            prediction = 'scissors'
+            if i > 2:
+                if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')] > \
+                        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')]:
+                    prediction = 'rock'
+                else:
+                    prediction = 'paper'
 
-        if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')] > combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')]:
-            prediction = 'paper'
-        else:
-            prediction = 'rock'
+                if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')] > \
+                        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')]:
+                    prediction = 'rock'
+                else:
+                    prediction = 'scissors'
 
-        if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')] > combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')]:
-            prediction = 'paper'
-        else:
-            prediction = 'scissors'
+                if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')] > \
+                        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')]:
+                    prediction = 'paper'
+                else:
+                    prediction = 'rock'
 
-        if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')] > combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')]:
-            prediction = 'scissors'
-        else:
-            prediction = 'rock'
+                if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')] > \
+                        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')]:
+                    prediction = 'paper'
+                else:
+                    prediction = 'scissors'
 
-        if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')] > combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')]:
-            prediction = 'scissors'
-        else:
-            prediction = 'paper'
-    if computer_choice == user_choice:
-        correct_count += 1
-    combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, user_choice)] += 1
+                if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')] > \
+                        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')]:
+                    prediction = 'scissors'
+                else:
+                    prediction = 'rock'
 
-    prev_prev_prev_choice = prev_prev_choice
-    prev_prev_choice = prev_choice
-    prev_choice = user_choice
+                if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')] > \
+                        combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')]:
+                    prediction = 'scissors'
+                else:
+                    prediction = 'paper'
+            if computer_choice == user_choice:
+                correct_count += 1
+            combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, user_choice)] += 1
 
-print(f"Computer wins : {correct_count} times.")
+            prev_prev_prev_choice = prev_prev_choice
+            prev_prev_choice = prev_choice
+            prev_choice = user_choice
+
+        print(f"Computer wins : {correct_count} times.")
+
+game = Game()
+game.collecting_history()
+exit()
+
+
