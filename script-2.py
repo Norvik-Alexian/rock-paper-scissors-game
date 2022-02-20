6259dfdcd73f5126aa642c7f4aba8c4f7644614d
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-02-20 19:44:33 +0400

    update OOP aspect of the code

--- script-2.py
+++ script-2.py
@@ -11,6 +11,7 @@ class Game:
         self.prev_choice = None
         self.prev_prev_choice = None
         self.prev_prev_prev_choice = None
+        self.prediction = None
 
     def collecting_history(self):
         combinations = list(itertools.product(game_choices, repeat=4))
@@ -30,8 +31,12 @@ class Game:
             self.prev_prev_choice = self.prev_choice
             self.prev_choice = user_choice
 
+        return combination_history, computer_choice
+
     def guess_possiblity(self):
-        prediction = None
+        computer_choice = self.collecting_history()[1]
+        combination_history = self.collecting_history()[0]
+
         for i in range(7):
             user_choice = input('Enter your choice: ')
             print(f'Computer chose => {computer_choice}')
@@ -39,51 +44,48 @@ class Game:
             if i > 2:
                 if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')] > \
                         combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')]:
-                    prediction = 'rock'
+                    self.prediction = 'rock'
                 else:
-                    prediction = 'paper'
+                    self.prediction = 'paper'
 
                 if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')] > \
                         combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')]:
-                    prediction = 'rock'
+                    self.prediction = 'rock'
                 else:
-                    prediction = 'scissors'
+                    self.prediction = 'scissors'
 
                 if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')] > \
                         combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')]:
-                    prediction = 'paper'
+                    self.prediction = 'paper'
                 else:
-                    prediction = 'rock'
+                    self.prediction = 'rock'
 
                 if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')] > \
                         combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')]:
-                    prediction = 'paper'
+                    self.prediction = 'paper'
                 else:
-                    prediction = 'scissors'
+                    self.prediction = 'scissors'
 
                 if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')] > \
                         combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'rock')]:
-                    prediction = 'scissors'
+                    self.prediction = 'scissors'
                 else:
-                    prediction = 'rock'
+                    self.prediction = 'rock'
 
                 if combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'scissors')] > \
                         combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, 'paper')]:
-                    prediction = 'scissors'
+                    self.prediction = 'scissors'
                 else:
-                    prediction = 'paper'
+                    self.prediction = 'paper'
             if computer_choice == user_choice:
-                correct_count += 1
+                self.correct_count += 1
             combination_history[(prev_prev_prev_choice, prev_prev_choice, prev_choice, user_choice)] += 1
 
             prev_prev_prev_choice = prev_prev_choice
             prev_prev_choice = prev_choice
             prev_choice = user_choice
 
-        print(f"Computer wins : {correct_count} times.")
+        print(f"Computer wins : {self.correct_count} times.")
 
 game = Game()
 game.collecting_history()
-exit()
-
-
